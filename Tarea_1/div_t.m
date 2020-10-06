fcb55e07ae40e79d7583eb569ca793ecfca0a3ca
Deiber <Deibergv@gmail.com>
2020-10-06 14:46:03 -0600

    files add

---
+++ Tarea_1/div_t.m
@@ -0,0 +1,36 @@
+%pkg load symbolic;
+%Función Transcendental de la división
+%
+function [Xk] = div_t(x,tol)
+  Xk=0;#Resultado
+  eps=2.2204*10^(-16); %Precisión relativa del punto flotante
+  error=tol+1; %Error
+  k=1;%Iteraciones
+  %-------------------------------------
+  %%Establecer el valor inicial de X0
+  if (factorial(80)<x<=factorial(100))
+    Xk=eps^15;
+  endif
+  if (factorial(60)<x<=factorial(80))
+    Xk=eps^11
+  endif
+  if (factorial(40)<x<=factorial(60))
+    Xk=eps^8;
+  endif
+  if (factorial(20)<x<=factorial(40))
+    Xk=eps^4;
+  endif
+  if (factorial(0)<x<=factorial(20))
+    Xk=eps^2;
+  endif
+  %----------------------------------
+  while error>=tol %Condición de parada error Mayor o igual que la tolerancia
+    Xk_nuevo=Xk*(2-(x*Xk));%%Aproximación de la división
+    error=abs((Xk_nuevo-Xk)/Xk_nuevo);%%Calcular error
+    Xk=Xk_nuevo;
+    k=k+1;%Iteraciones
+  endwhile
+endfunction
+
+
+#[Xk] = div_t(5,10^-3)
---
+++ Tarea_1/exp_t.m
@@ -0,0 +1,17 @@
+%
+%Función Transcendental de e^x
+%
+function [Sk] = exp_t(x,tol)
+  Sk=1;%Resultado
+  k=1;%iteracion
+  error=tol+1;
+  while error>=tol %Condición de parada error Mayor o igual que la tolerancia
+    Sk_nuevo = (x^k)*(div_t(factorial(k),tol))+Sk;
+    error=abs(Sk_nuevo-Sk); %Calcular error
+    Sk=Sk_nuevo;
+    k=k+1;
+  endwhile
+endfunction
+
+
+[Sk] = exp_t(1,10^-3)
