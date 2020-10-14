06ecec9252b5c5daf66fc50b78b93344c2a091c6
Gerald Valverde <gerald-_16@hotmail.com>
2020-10-14 12:43:21 -0600

    Tanh

--- Tarea_1/tanh.m
+++ Tarea_1/tanh.m
@@ -4,9 +4,5 @@
 %
 function [Sk] = tanh(x)
   tol=10^-8;
-  Sk=1;%Condicion inicial
-  k=1;%iteracion
-  error=tol+1;
-  while error>=tol %Condición de parada error Mayor o igual que la tolerancia
-    Sk_temp< 
+  Sk=sinh(x)*div_t(cosh(x))
 endfunction
