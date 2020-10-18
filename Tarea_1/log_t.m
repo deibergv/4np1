6c6c6c75d9938c15f56f3b3c95591e99b7fa26b3
Deiber <Deibergv@gmail.com>
2020-10-18 14:42:29 -0600

    Log with conditions and messages

--- Tarea_1/log_t.m
+++ Tarea_1/log_t.m
@@ -1,12 +1,20 @@
 %pkg load symbolic;
 %Función Transcendental de logaritmo base a de x: log_t(x,a)
-% Se sabe que loga(x)=ln(x)/ln(a)
+% Se sabe que log_a(x)=ln(x)/ln(a)
 %
 function [Sk] = log_t(x,a)
-  if (x>0 && a>0 && a=! 1) 
+  
+  if (x<0)
+    disp ("El argumento debe ser mayor a 0");
+  elseif (a<0) 
+    disp ("La base debe ser mayor a 0");
+  elseif (a== 1) 
+    disp ("La base debe ser diferente de 1");
+  elseif (x>0 && a>0 && a!= 1) 
     Sk=ln_t(x)*(div_t(ln_t(a)));
    endif
 endfunction
 
 
-%[Sk] = sin_t(90*pi/180,10^-3)
+
+% log_t(1,10)
