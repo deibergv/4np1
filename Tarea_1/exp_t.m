a1c02b84cddca2b76ea1629d981bbe545578dbd8
Gerald Valverde <gerald-_16@hotmail.com>
2020-10-18 17:47:28 -0600

    Caso exponente negativo

--- Tarea_1/exp_t.m
+++ Tarea_1/exp_t.m
@@ -1,17 +1,26 @@
 %
 %Función Transcendental de e^x
 %
-function [Sk] = exp_t(x)
+function Sk = exp_t(x)
   tol=10^-8;
   Sk=1;%Resultado
   k=1;%iteracion
   error=tol+1;
+  
   while error>=tol %Condición de parada error Mayor o igual que la tolerancia
+    abs(x);
     Sk_nuevo = (x^k)*(div_t(factorial(k)))+Sk;
     error=abs(Sk_nuevo-Sk); %Calcular error
     Sk=Sk_nuevo;
     k=k+1;
   endwhile
+  if(x==0)
+    Sk=1;
+  endif
+  elseif(x<0)
+    Sk=div_t(Sk);
+  end
+  
 endfunction
 
 
